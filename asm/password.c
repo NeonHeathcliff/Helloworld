4fb1629e79ddb486d47610f857de1f694cb71483
HeathcliffNeon <heathcliff1008@163.com>
2020-02-03 22:38:42 +0800

    msater

---
+++ asm/password.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#define PASSWORD "1234567"
+
+int verify_password(char *password){
+	int authenticated;
+	authenticated = strcmp(password,PASSWORD);
+    return authenticated;
+}
+
+int main() 
+{
+    int valid_flag = 0;
+    char password[1024];
+    while(1){
+        printf("please input password :                 ");
+        scanf("%s",password);
+        valid_flag = verify_password(password);
+        if(valid_flag){
+            printf("incorrect password\n\n");
+        }
+        else{
+            printf("Congratulation!you win!!!\n");
+            break;
+        }
+    }
+    return 0;
+}
